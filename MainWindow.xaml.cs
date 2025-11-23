7d7372d5cd446ffd94b2189b80f6933fb8e8929f
jstevens <jeremytstevensdev@gmail.com>
2025-11-22 21:06:05 -0600

    still fighting with data binding. how do i bind the monitor tab Content to the computer object?

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,26 +14,6 @@ namespace FanGB
     /// </summary>
     public partial class MainWindow : Window
     {
-        public class KeyValuePair
-        { 
-            public KeyValuePair(Identifier identifier, string key, string value)
-            {
-                ID = identifier;
-                Key = key;
-                Value = value;
-            }
-
-            public Identifier ID { get; }
-
-            public string Key { get; set; }
-
-            public string Value {  get; set; }
-
-            
-        }
-
-        private ObservableCollection<KeyValuePair> MonitorData = new ObservableCollection<KeyValuePair>();
-
         public MainWindow()
         {
             InitializeComponent();
@@ -52,54 +32,9 @@ namespace FanGB
             computer.Open();
             computer.Accept(new UpdateVisitor());
 
-            foreach (IHardware hardware in computer.Hardware)
-            {
-                string hardwareName = hardware.Name;
-                Grid hardwareGrid = new Grid();
-                hardwareGrid.ShowGridLines = true;
-                //hardwareGrid.Name = hardwareName;
-                HardwareList.ColumnDefinitions.Add(new ColumnDefinition());
+            DataContext = computer;
 
-                HardwareList.Children.Add(hardwareGrid);
-
-                foreach (IHardware subhardware in hardware.SubHardware)
-                {
-                    string subhardwareName = subhardware.Name;
-                    Grid subhardwareGrid = new Grid();
-                    subhardwareGrid.ShowGridLines = true;
-                    //subhardwareGrid.Name = subhardwareName;
-                    subhardwareGrid.ColumnDefinitions.Add(new ColumnDefinition());
-                    subhardwareGrid.ColumnDefinitions.Add(new ColumnDefinition());
-
-                    hardwareGrid.Children.Add(subhardwareGrid);
-
-                    StackPanel sensorNamesPanel = new StackPanel();
-                    StackPanel sensorValuesPanel = new StackPanel();
-
-                    subhardwareGrid.Children.Add(sensorNamesPanel);
-                    subhardwareGrid.Children.Add(sensorValuesPanel);
-
-                    foreach (ISensor sensor in subhardware.Sensors)
-                    {
-
-                        if (sensor.SensorType == SensorType.Fan || sensor.SensorType == SensorType.Control)
-                        {
-                            string sensorName = sensor.Name;
-                            string sensorValue = sensor.Value.ToString();
-
-                            Grid.SetColumn(sensorNamesPanel, 0);
-                            Grid.SetColumn(sensorValuesPanel, 1);                            
-
-                            sensorNamesPanel.Children.Add(new TextBlock() { Text = sensorName });
-                            sensorValuesPanel.Children.Add(new TextBlock() { Text = sensorValue });
-
-                        }
-
-                    }
-                }
-            }
-
-        computer.Close();
+            computer.Close();
         }
     }
 }
